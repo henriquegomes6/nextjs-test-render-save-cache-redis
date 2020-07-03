c6d1b4e56a32a7c7ffbe340ee4d72600823c20a8
henriquegomes6 <henriquegomes6@hotmail.com>
2020-07-03 03:19:40 -0300

    add index page react

---
+++ src/pages/index.tsx
@@ -0,0 +1,52 @@
+import { Redirect } from 'react-router';
+import { Link, Route, Switch } from 'react-router-dom';
+
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+function NotFound() {
+  return <h2>Not found</h2>;
+}
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about/">About</Link>
+          </li>
+          <li>
+            <Link to="/users/">Users</Link>
+          </li>
+          <li>
+            <Link to="/people/">People</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Switch>
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+        <Redirect from="/people/" to="/users/" />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
